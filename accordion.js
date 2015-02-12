29d7b2f1dc1cab687ba8a507d393c3410cb7a0aa
Josh Carrico <jcarrico138@gmail.com>
2015-02-12 11:15:40 -0500

    Still not getting the transition, but overall look is set.

---
+++ accordion.js
@@ -0,0 +1,30 @@
+var pullCard = function(info) {
+
+	var cardHtml = "<div class='accordion'><div class='info'>" + info + "</div></div>";
+
+	var $cardInfo = $(cardHtml);
+
+	$("#sections").append($cardInfo)
+
+	$cardInfo.on("click", function(evt){
+
+		$(".accordion").removeClass("opened");
+
+		var $openedCard = $(this);
+
+		$openedCard.toggleClass("opened");
+	});
+}
+
+
+
+$(".accordion").on("click", function(evt){
+
+
+  var $clickedHeader = $(this);
+
+  $(".sections").removeClass("active");
+
+  $clickedHeader.find(".sections").addClass("active");
+
+});
